11364957c6a0741849f9cbb69987cc88e99446d9
valentinborys <valentin.borys97@gmail.com>
2024-04-10 15:50:07 +0300

    My 6-th homework

---
+++ lecture_06/homework_6_1.py
@@ -0,0 +1,18 @@
+# Завдання 1 - Рахування унікальних символів в строці
+
+unique = (str(input("Введіть значення: ")))
+
+unique_list = []
+
+for letter in unique:
+    if letter == letter:
+        unique_list.append(letter.lower())
+
+set_unique = set(unique_list)
+
+if len(set_unique) >= 10:
+    print("Кількість унікальних символів більше чи дорівнює десяти:", True)
+else:
+    print("Кількість унікальних символів менше десяти:", False)
+
+# print(set_unique)
---
+++ lecture_06/homework_6_2.py
@@ -0,0 +1,13 @@
+# Завдання 2 - Цикл “Дочекайся літери”
+
+word = input('Введіть текст з літерою "h" або "H": ').lower()
+char_to_check = "h"
+
+while True:
+    if char_to_check in word:
+        print('В тексті є літера "h" або "H"')
+        break
+    else:
+        print('В тексті немає літери "h" або "H"')
+        word = input('\nВведіть слово з літерою "h": ').lower()
+
