09ebb507febbc7de1311fb67170c7ea1887984a1
valentinborys <valentin.borys97@gmail.com>
2024-05-13 17:33:41 +0300

    Update

--- lecture_13/Task.py
+++ lecture_13/Task.py
@@ -16,73 +16,73 @@ class Shape(ABC):
 class Rectangle(Shape):
     def __init__(self, width, height):
         super().__init__()
-        self.width = width
-        self.height = height
+        self._width = width
+        self._height = height
 
     def area(self):
-        return self.width * self.height
+        return self._width * self._height
 
     def perimeter(self):
-        return 2 * (self.width + self.height)
+        return 2 * (self._width + self._height)
 
 
 class Square(Shape):
     def __init__(self, side):
         super().__init__()
-        self.side = side
+        self._side = side
 
     def area(self):
-        return self.side * self.side
+        return self._side * self._side
 
     def perimeter(self):
-        return self.side * 4
+        return self._side * 4
 
 
 class Circle(Shape):
     def __init__(self, radius):
         super().__init__()
-        self.radius = radius
+        self._radius = radius
 
     def area(self):
-        return round(math.pi * (self.radius * self.radius), 2)
+        return round(math.pi * (self._radius * self._radius), 2)
 
     def perimeter(self):
-        return round((2 * math.pi * self.radius), 2)
+        return round((2 * math.pi * self._radius), 2)
 
 
 width = int(input("Введіть ширину прямокутника: "))
 height = int(input("Введіть висоту прямокутника: "))
 
-# Площа для прямокутника
 area_rect = Rectangle(width, height)
-print(f"Площа прямокутника: {area_rect.area()}")
 
-# Периметр для прямокутника
-perimeter_rect = Rectangle(width, height)
-print(f"Периметр прямокутника: {perimeter_rect.perimeter()}")
+# Площа та периметр прямокутника
+print(f"Площа прямокутника: {area_rect.area()}")
+print(f"Периметр прямокутника: {area_rect.perimeter()}")
 
 print("")
 side = int(input("Введіть сторону квадрату: "))
 
-# Площа та периметр для квадрата
 square = Square(side)
+
+# Площа та периметр квадрату
 print(f"Площа квадрату: {square.area()}")
 print(f"Периметр квадрату: {square.perimeter()}")
 
-
-# Площа та периметр для кругу
 print("")
 radius = float(input("Введіть радіус кола: "))
+
 circle = Circle(radius)
+
+# Площа та периметр кола
 print(f"Площа кола: {circle.area()}")
 print(f"Периметр/довжина кола: {circle.perimeter()}")
 
 print("")
 
-# Виведення через цикл:
+# Вивід через цикл
 print("Всі відповіді:")
-all_answers = [area_rect.area(), perimeter_rect.perimeter(), square.area(), square.perimeter(), circle.area(), circle.perimeter()]
-test = ["Площа прямокутника", "Периметр прямокутника", "Площа квадрата", "Периметр квадрата", "Площа кола", "Периметр/довжина прямокутника"]
+all_answers = [area_rect.area(), area_rect.perimeter(), square.area(), square.perimeter(), circle.area(), circle.perimeter()]
+test = ["Площа прямокутника", "Периметр прямокутника", "Площа квадрата", "Периметр квадрата", "Площа кола", "Периметр/довжина кола"]
 
 for i in range(len(all_answers)):
     print(f"{test[i]}: {all_answers[i]}")
