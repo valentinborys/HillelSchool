f2b16571285045a0b3eb267b768cf62a2894a7cc
valentinborys <valentin.borys97@gmail.com>
2024-05-14 22:39:36 +0300

    Update

---
+++ lecture_14/Test.py
@@ -0,0 +1,11 @@
+from Task import TeamLead
+
+team_lead = TeamLead("Валентин", 100000, "QA", "Python", 5)
+
+assert hasattr(team_lead, 'name')
+assert hasattr(team_lead, 'salary')
+assert hasattr(team_lead, 'department')
+assert hasattr(team_lead, 'programming_language')
+assert hasattr(team_lead, 'team_size')
+
+print("Вітаю! Тест пройдено, всі атрибути в класі \"TeamLead\" є:)")
